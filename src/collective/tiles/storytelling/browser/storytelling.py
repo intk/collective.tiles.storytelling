0dbf0d77dd050116d6cfce256949c43a52cdc475
andreesg <andre@intk.com>
2021-01-20 13:53:04 +0100

    Configure side text template. Add options to choose image and alignment

--- src/collective/tiles/storytelling/browser/storytelling.py
+++ src/collective/tiles/storytelling/browser/storytelling.py
@@ -8,9 +8,13 @@ from plone.tiles.directives import ignore_querystring
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.utils import safe_unicode
 from zope import schema
-
-
-TEMPLATE = "<div class='side-text-slide'></div>"
+from plone.i18n.normalizer.interfaces import IIDNormalizer
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.component import getUtility
+from plone.app.textfield import RichText
+from plone.app.z3cform.widget import RelatedItemsFieldWidget
+from plone.autoform import directives as form
+from plone import api
 
 class ISideTextTile(Schema):
 
@@ -43,3 +47,57 @@ class SideTextTile(Tile):
         else:
             content = u'<p>This is a side text slide - Default text</p>'
         return u"<html><body>%s</body></html>" % safe_unicode(content)
+
+class SideTextSlideTile(Tile):
+    template = ViewPageTemplateFile('templates/sidetext.pt')
+
+    def __call__(self):
+        self.update()
+        return self.template()
+
+    def update(self):
+        self.title = self.data.get('title', '')
+        self.body = self.data.get('body', '')
+        self.alignment = self.data.get('image_alignment', '')
+        self.tile_id = getUtility(IIDNormalizer).normalize(self.title)
+        img_uid = self.data.get('image', None)
+        
+        if img_uid is None:
+            self.image_url = None
+        else:
+            self.image_url = api.content.get(UID=img_uid).absolute_url()
+
+class ISideTextSlideTile(Schema):
+    title = schema.TextLine(
+        title=_(u"Slide title"),
+        required=True,
+    )
+    body = RichText(
+        title=_(u"Slide body text"),
+        description=_(u"Text that appears on the side of the slide"),
+        required=True,
+    )
+    image = schema.Choice(
+        title=_(u"Select an image"),
+        required=True,
+        vocabulary='plone.app.vocabularies.Catalog',
+    )
+    form.widget(
+        'image',
+        RelatedItemsFieldWidget,
+        vocabulary='plone.app.vocabularies.Catalog',
+        pattern_options={
+            'recentlyUsed': True,
+            'selectableTypes': ['Image'],
+        }
+    )
+
+    image_alignment = schema.Choice(
+        title=_(u"Choose image alignment"),
+        description=_(u"Image is aligned to the left by default"),
+        values=[_(u'Left'), _(u'Right')],
+        default=_(u'Left'),
+        required=False
+    )
+
+
