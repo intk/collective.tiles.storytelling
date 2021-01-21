4fea7f0b9dec4ad3e6adbddf0d47422a9cb7b4d5
andreesg <andreslb1@gmail.com>
2021-01-21 14:33:07 +0100

    Clean tile declaration. Add correct name.

--- src/collective/tiles/storytelling/browser/storytelling.py
+++ src/collective/tiles/storytelling/browser/storytelling.py
@@ -16,39 +16,6 @@ from plone.app.z3cform.widget import RelatedItemsFieldWidget
 from plone.autoform import directives as form
 from plone import api
 
-class ISideTextTile(Schema):
-
-    ignore_querystring('content')
-    primary('content')
-    content = schema.Text(
-        title=_(u'HTML'),
-        required=True,
-    )
-
-class SideTextTile(Tile):
-    """
-    A persistent HTML content tile that can be used for
-    re-usable layouts in the mosaic editor
-    """
-
-    def __call__(self):
-        content = self.data.get('content')
-        if content:
-            # only transform is not rendering for layout editor
-            if (not self.request.get('_layouteditor') or
-                    ISubRequest.providedBy(self.request)):
-                transforms = getToolByName(self.context, 'portal_transforms')
-                data = transforms.convertTo(
-                    'text/x-html-safe',
-                    safe_unicode(content), mimetype='text/html',
-                    context=self.context
-                )
-                content = data.getData()
-        else:
-            content = u'<p>This is a side text slide - Default text</p>'
-        return u"<html><body>%s</body></html>" % safe_unicode(content)
-
-
 class SideTextSlideTile(Tile):
     template = ViewPageTemplateFile('templates/sidetext.pt')
 
@@ -62,7 +29,7 @@ class SideTextSlideTile(Tile):
         self.alignment = self.data.get('image_alignment', '')
         self.selected_image = self.data.get('selected_image', '')
         self.tile_id = getUtility(IIDNormalizer).normalize(self.title)
-        
+
 
 class ISideTextSlideTile(Schema):
     title = schema.TextLine(
